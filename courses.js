24aaa0de02d506acaa5d610ca236ce3352d3602e
My Bergquist Rajic <mybergqvist@gmail.com>
2022-03-27 10:53:56 +0200

    Began working with corses

---
+++ courses.js
@@ -0,0 +1,68 @@
+"use strict"
+
+let courseResults = document.querySelector(".courses");
+
+function filterCoursesByName() {
+    courseResults.innerHTML = "";
+    let courses = DATABASE.courses;
+    let result = courses.filter(course => course.title.toLowerCase().includes(getInputValue()));
+    createCourses(result);
+}
+
+
+function createCourses(course) {
+    for (let i = 0; i < course.length; i++) {
+        createCourse(course, i)
+    }
+}
+
+function createCourse(course, i) {
+    let courseItem = document.createElement("div");
+    courseItem.classList.add("course");
+    courseItem.id = `${course[i].courseId}`
+    courseItem.innerHTML = `
+    <h2>${course[i].title} (${course[i].totalCredits} credits)</h2>
+    <div id="${course[i].courseId}-responsible">Course responsible:</div>
+    <div id="${course[i].courseId}-teachers">Course responsible:</div>
+    <div id="${course[i].courseId}-students">Students:</div>`
+    courseResults.appendChild(courseItem);
+    courseResponsible(course, i)
+    courseTeachers(course, i)
+    // courseStudents(course, i)
+}
+
+
+function courseResponsible(course, i) {
+    let responsible = document.createElement("div");
+    responsible.classList.add("courseresponsible");
+    for (let teacher of DATABASE.teachers) {
+        if (course[i].courseResponsible == teacher.teacherId) {
+            responsible.innerHTML = `
+                <p>${teacher.firstName} ${teacher.lastName} (${teacher.post})</p>`;
+        }
+    }
+    document.getElementById(`${course[i].courseId}-responsible`).appendChild(responsible);
+}
+
+function courseTeachers(course, i) {
+    for (let courseTeacher of course[i].teachers) {
+        let teacher = document.createElement("div");
+        teacher.classList.add("courseteacher");
+        let teacherInfo = document.createElement("p");
+        for (let databaseTeacher of DATABASE.teachers) {
+            if (courseTeacher == databaseTeacher.teacherId) {
+                teacherInfo.innerHTML = `
+                ${databaseTeacher.firstName} ${databaseTeacher.lastName} (${databaseTeacher.post})`
+            }
+        }
+        teacher.appendChild(teacherInfo);
+        document.getElementById(`${course[i].courseId}-teachers`).appendChild(teacher);
+    }
+
+}
+
+// Function that gets the value from the input field
+function getInputValue() {
+    let input = document.getElementById('courseinput').value.toLowerCase();
+    return input;
+}
